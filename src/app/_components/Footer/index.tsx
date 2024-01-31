cb301b39a05edf4b15bbb8ae2fed204ddfd1883a
junghyunbak <jeong5728@gmail.com>
2024-01-31 15:00:45 +0900

    chore: tailwind divide util 스타일 제거

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -1,13 +1,17 @@
 export function Footer() {
   return (
-    <footer className="flex items-center justify-center w-full border-t border-black border-dashed divide-x divide-black p-2.5 [&_a]:px-2.5">
+    <footer className="flex items-center justify-center w-full border-t border-black border-dashed gap-4 p-2.5">
       <a href="https://github.com/junghyunbak" target="__blank">
         Github
       </a>
 
+      <div className="h-4 border-l border-black" />
+
       <a href="https://www.instagram.com/__cxxlxxhxxn/" target="__blank">
         Instagram
       </a>
+
+      <div className="h-4 border-l border-black" />
 
       <a href="mailto:jeong5728@gmail.com">Gmail</a>
     </footer>
