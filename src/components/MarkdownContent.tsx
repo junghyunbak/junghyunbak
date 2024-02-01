21c1952e77da65802f26b12c6dbf0fc92c5939c0
junghyunbak <jeong5728@gmail.com>
2024-02-02 01:23:40 +0900

    style: markdown 스타일 수정

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -2,7 +2,8 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vs } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "./MarkdownContent.css";
 
 interface MarkdownContentProps {
   content: string;
@@ -10,31 +11,33 @@ interface MarkdownContentProps {
 
 export function MarkdownContent({ content }: MarkdownContentProps) {
   return (
-    <Markdown
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
-      components={{
-        code({ inline, children, className, node, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
+    <div className="markdown">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          code({ inline, children, className, node, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
 
-          return !inline && match ? (
-            <SyntaxHighlighter
-              {...props}
-              PreTag="div"
-              language={match[1]}
-              style={vs}
-            >
-              {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code {...props} className={className}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {content}
-    </Markdown>
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                PreTag="div"
+                language={match[1]}
+                style={vscDarkPlus}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </Markdown>
+    </div>
   );
 }
