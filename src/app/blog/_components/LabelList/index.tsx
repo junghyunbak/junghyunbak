91b9880e95a33cbcfd75790e6dd876a9cb3afa7f
junghyunbak <jeong5728@gmail.com>
2024-02-01 20:21:54 +0900

    chore: 라벨 리스트 아이템 삭제 및 올바른 경로로 라우트하도록 수정

--- src/app/blog/_components/LabelList/index.tsx
+++ src/app/blog/_components/LabelList/index.tsx
@@ -1,5 +1,5 @@
-import { LabelListItem } from "./LabelListItem";
 import { type LabelListResponse } from "@/types/label";
+import Link from "next/link";
 
 interface LabelListProps {
   labels: LabelListResponse["data"];
@@ -9,7 +9,16 @@ export function LabelList({ labels }: LabelListProps) {
   return (
     <ul className="flex flex-wrap gap-1.5 mt-11">
       {labels.map((label) => {
-        return <LabelListItem key={label.id} label={label} />;
+        return (
+          <li key={label.id}>
+            <Link
+              className="border border-primary rounded-sm p-[0.3125rem] text-sm leading-4 text-primary"
+              href={`/blog?label=${label.name}`}
+            >
+              {`#${label.name}`}
+            </Link>
+          </li>
+        );
       })}
     </ul>
   );
