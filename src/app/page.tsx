fe1c0cf297625fdce3ee6fca1be25c104be5ed8d
junghyunbak <jeong5728@gmail.com>
2024-02-02 02:59:27 +0900

    feat: 소개 페이지에 markdown 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { MarkdownContent } from "@/components/MarkdownContent";
 import markdown from "@/docs/about.md";
 
 export default function Home() {
-  return <main>{markdown}</main>;
+  return (
+    <main className="mt-6">
+      <MarkdownContent content={markdown} />
+    </main>
+  );
 }
