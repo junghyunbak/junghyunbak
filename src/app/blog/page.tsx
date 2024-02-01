93867d79a402e3a5105a360d4a819a4cebba7d4e
junghyunbak <jeong5728@gmail.com>
2024-02-01 18:35:50 +0900

    fix: 리팩토링 과정에서 빠진 export default 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import { getIssues, getLabels } from "@/apis";
 import { IssueList } from "./_components/IssueList";
 import { LabelList } from "./_components/LabelList";
 
-export async function Blog() {
+export default async function Blog() {
   const issues = await getIssues({ per_page: 5 });
   const labels = await getLabels({ per_page: 2 });
 
