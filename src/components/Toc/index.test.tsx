8ae6c6586a91a48f248c0361d387fe3f75eba894
junghyunbak <jeong5728@gmail.com>
2024-02-10 12:43:17 +0900

    chore: toc 컴포넌트 테스트코드 정리

--- src/components/Toc/index.test.tsx
+++ src/components/Toc/index.test.tsx
@@ -1,23 +1,30 @@
 import { Toc } from ".";
 import { render, screen } from "@testing-library/react";
 
-describe("TOC 컴포넌트", () => {
-  it("TOC 컴포넌트가 올바르게 렌더링되는지 확인한다.", () => {
-    const markdownText = "## h2\n\n안녕하세요\n\n### h3\n\n반갑습니다.";
+const markdownText = `
+## h2
 
-    render(<Toc markdown={markdownText} />);
+안녕하세요,
 
-    const linkEl = screen.getByRole("link", { name: "h2" });
+### h3
 
-    expect(linkEl.getAttribute("href")).toEqual("#h2");
-  });
+반갑습니다.
 
-  it("마크다운 요소 중 heading 4 요소는 렌더링되지 않아야 한다.", () => {
-    const markdownText =
-      "## h2\n\n안녕하세요\n\n### h3\n\n반갑습니다.\n\n#### h4";
+#### h4
 
-    render(<Toc markdown={markdownText} />);
+테스트입니다.
+`;
 
-    expect(screen.queryByRole("link", { name: "h4 " })).toEqual(null);
-  });
+it("TOC 컴포넌트가 올바르게 렌더링되는지 확인한다.", () => {
+  render(<Toc markdown={markdownText} />);
+
+  const linkEl = screen.getByRole("link", { name: "h2" });
+
+  expect(linkEl.getAttribute("href")).toEqual("#h2");
+});
+
+it("마크다운 요소 중 heading 4 요소는 렌더링되지 않아야 한다.", () => {
+  render(<Toc markdown={markdownText} />);
+
+  expect(screen.queryByRole("link", { name: "h4" })).toEqual(null);
 });
