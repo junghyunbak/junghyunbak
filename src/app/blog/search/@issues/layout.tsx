d733fe84d89c6681f518b14cd7e10409775ef6e8
junghyunbak <jeong5728@gmail.com>
2024-02-20 20:27:49 +0900

    chore: 검색 페이지 suspense 사용이유 주석 설명

--- src/app/blog/search/@issues/layout.tsx
+++ src/app/blog/search/@issues/layout.tsx
@@ -20,6 +20,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="my-11">
+        {/**
+         * https://github.com/vercel/next.js/discussions/50563
+         *
+         * suspense가 최초 1회만 동작하는 이슈가 있어
+         * `loading.tsx` 파일 대신 직접 suspense 컴포넌트를 사용함.
+         */}
         <Suspense key={searchParams.toString()} fallback={<p>로딩 중...</p>}>
           {children}
         </Suspense>
