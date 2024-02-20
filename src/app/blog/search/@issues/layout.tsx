fd92b36136ff840eb8cff0c7affea893a1041c12
junghyunbak <jeong5728@gmail.com>
2024-02-20 18:40:37 +0900

    fix: refetch시 suspense 동작하지 않는 이슈 수정

--- src/app/blog/search/@issues/layout.tsx
+++ src/app/blog/search/@issues/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
@@ -14,9 +15,15 @@ const queryClient = new QueryClient({
 });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const searchParams = useSearchParams();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="my-11">{children}</div>
+      <div className="my-11">
+        <Suspense key={searchParams.toString()} fallback={<p>로딩 중...</p>}>
+          {children}
+        </Suspense>
+      </div>
 
       {process.env.NODE_ENV !== "production" && <ReactQueryDevtools />}
     </QueryClientProvider>
--- src/app/blog/search/@issues/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-  return <p>로딩 중...</p>;
-}
