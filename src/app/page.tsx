a3b37991d312b46f73daecac73777a3afafa5c49
junghyunbak <jeong5728@gmail.com>
2024-02-25 13:44:56 +0900

    refactor: 소개 페이지에 Issue 컴포넌트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { ISSUE_ABOUT_NUMBER, REPO_OWNER, REPO_NAME } from "@/apis";
 import { Metadata } from "next";
 import { Hits } from "@/components/Hits";
-import { Utterances } from "@/components/Utterances";
-import { Markdown } from "@/components/Markdown";
 import { Header } from "@/components/Header";
 import Link from "next/link";
+import { Issue } from "@/components/widgets/Issue";
+import { extractImageUrlsFromMarkdown, getImageUrlToPreviewImageData } from "@/utils/image";
 
 export const revalidate = 60;
 
@@ -31,6 +31,9 @@ export default async function Home() {
     .then((res) => res.json())
     .catch(() => [])) as Issues;
 
+  const imageUrls = extractImageUrlsFromMarkdown(issue?.body || "");
+  const imageUrlToPreviewImage = await getImageUrlToPreviewImageData(imageUrls);
+
   return (
     <>
       <Header currentPage="소개" />
@@ -38,34 +41,12 @@ export default async function Home() {
       <main className="mt-6 max-md:p-3">
         <Hits path="/" />
 
-        <Markdown markdown={issue?.body || ""} />
-
-        <div className="markdown">
-          <h2>최근 업데이트 한 게시물</h2>
-
-          <ul>
-            {recentIssues.map((issue) => {
-              return (
-                <li key={issue.id}>
-                  <Link href={`/post/${issue.number}`}>
-                    {issue.title}
-                    <span className="text-gray-600">{` (${MillisecondsToElapsedTimeString(
-                      Date.now() - new Date(issue.updated_at).getTime()
-                    )} 전)`}</span>
-                  </Link>
-                </li>
-              );
-            })}
-
-            <li>
-              <Link href={`/blog/search?sort=updated&order=desc`}>
-                ...더보기
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <Utterances issueNumber={ISSUE_ABOUT_NUMBER} />
+        <Issue
+          markdown={issue?.body || ""}
+          number={ISSUE_ABOUT_NUMBER}
+          imageUrlToPreviewImage={imageUrlToPreviewImage}
+          moreContent={<RecentIssueList recentIssues={recentIssues}/>}
+        />
       </main>
     </>
   );
@@ -109,4 +90,35 @@ function MillisecondsToElapsedTimeString(ms: number) {
   }
 
   return `${year}년`;
+}
+
+interface RecentIssueListProps {
+  recentIssues: Issues;
+}
+
+function RecentIssueList({ recentIssues }: RecentIssueListProps) {
+  return (
+    <div className="markdown">
+      <h2>최근 업데이트 한 게시물</h2>
+
+      <ul>
+        {recentIssues.map((issue) => {
+          return (
+            <li key={issue.id}>
+              <Link href={`/post/${issue.number}`}>
+                {issue.title}
+                <span className="text-gray-600">{` (${MillisecondsToElapsedTimeString(
+                  Date.now() - new Date(issue.updated_at).getTime()
+                )} 전)`}</span>
+              </Link>
+            </li>
+          );
+        })}
+
+        <li>
+          <Link href={`/blog/search?sort=updated&order=desc`}>...더보기</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
