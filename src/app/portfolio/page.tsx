d37f3fd5cbeeb57cb0d2d6c79bc3d64e684bcc0a
junghyunbak <jeong5728@gmail.com>
2024-02-25 13:36:31 +0900

    refactor: 포트폴리오 페이지에 이슈 컴포넌트 적용

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,8 +1,11 @@
-import { Markdown } from "@/components/Markdown";
 import { apiService, ISSUE_PORTFOLIO_NUMBER } from "@/apis";
 import { Metadata } from "next";
 import { Hits } from "@/components/Hits";
-import { Toc } from "@/components/Toc";
+import { Issue } from "@/components/widgets/Issue";
+import {
+  extractImageUrlsFromMarkdown,
+  getImageUrlToPreviewImageData,
+} from "@/utils/image";
 
 export const metadata: Metadata = {
   title: "Portfolio | 개발자 박정현",
@@ -11,13 +14,18 @@ export const metadata: Metadata = {
 export default async function Portfolio() {
   const issue = await apiService.getAnIssue(ISSUE_PORTFOLIO_NUMBER.toString());
 
+  const imageUrls = extractImageUrlsFromMarkdown(issue?.body || "");
+  const imageUrlToPreviewImage = await getImageUrlToPreviewImageData(imageUrls);
+
   return (
     <div className="mt-6">
       <Hits path="/portfolio" />
 
-      <Toc markdown={issue?.body || ""} maxDepth={2} />
-
-      <Markdown markdown={issue?.body || ""} />
+      <Issue
+        markdown={issue?.body || ""}
+        number={ISSUE_PORTFOLIO_NUMBER}
+        imageUrlToPreviewImage={imageUrlToPreviewImage}
+      />
     </div>
   );
 }
