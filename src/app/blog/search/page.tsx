15a5cfac5c9d9f0168e1eba92cdc2b48bfff61de
junghyunbak <jeong5728@gmail.com>
2024-02-26 23:43:04 +0900

    chore: 파비콘 추가 & 검색 페이지 title 메타태그 추가

--- src/app/blog/search/page.tsx
+++ src/app/blog/search/page.tsx
@@ -1,3 +1,11 @@
+import { type Metadata } from "next";
+
+export function generateMetadata(): Metadata {
+  return {
+    title: "Blog - 검색 | 개발자 박정현",
+  };
+}
+
 export default function Search() {
   return null;
 }
