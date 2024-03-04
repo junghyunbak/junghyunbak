1f16936455e66360f50d0e15ab27c2cd775e2ed1
junghyunbak <jeong5728@gmail.com>
2024-03-04 21:41:19 +0900

    chore: post comment modal 에서 이미지 최적화를 사용하지 않는 이유 주석 추가

--- src/app/post/[slug]/@modal/(..)comment/[id]/page.tsx
+++ src/app/post/[slug]/@modal/(..)comment/[id]/page.tsx
@@ -4,6 +4,14 @@ import { RouteModal } from "@/components/core/RouteModal";
 import { ScrollYWithPaddingLayout } from "@/components/layout/ScrollYWithPaddingLayout";
 import { Markdown } from "@/components/core/Markdown";
 
+// BUG: https://github.com/vercel/next.js/issues/52842
+
+/**
+ * `intercepting route`를 위한 parallel 경로에서 정적 페이지 생성이 되지 않는 이슈가 존재한다.
+ *
+ * 이러한 상황에서 이미지 데이터(너비, 높이)를 사전에 불러와 활용하는 이미지 최적화 작업이 매 요청마다 이루어질 경우,
+ * 로딩시간이 매우 오래 걸리기 때문에 이미지 최적화 옵션을 비활성화 처리함.
+ */
 export default async function CommentModal({
   params: { id },
 }: {
