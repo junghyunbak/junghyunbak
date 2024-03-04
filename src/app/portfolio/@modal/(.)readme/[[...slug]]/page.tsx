9f5fa4f21ebb31ee55d32ce679106b057207e9e8
junghyunbak <jeong5728@gmail.com>
2024-03-04 21:55:13 +0900

    refactor: 레포지토리 readme 컨텐츠 컴포넌트 제거

--- src/app/portfolio/@modal/(.)readme/[[...slug]]/page.tsx
+++ src/app/portfolio/@modal/(.)readme/[[...slug]]/page.tsx
@@ -1,10 +1,20 @@
 import { RouteModal } from "@/components/core/RouteModal";
-import { ReadmeHeader } from "@/app/portfolio/readme/[[...slug]]/_components/ReadmeHeader";
-import { ReadmeContent } from "@/app/portfolio/readme/[[...slug]]/_components/ReadmeContent";
+import { ReadmeHeader } from "@/app/portfolio/@modal/(.)readme/[[...slug]]/_components/ReadmeHeader";
 import { apiService } from "@/apis";
 import { ModalLayout } from "@/components/layout/ModalLayout";
 import { ScrollYWithPaddingLayout } from "@/components/layout/ScrollYWithPaddingLayout";
+import { Markdown } from "@/components/core/Markdown";
+import base64 from "base-64";
+import utf8 from "utf8";
 
+// BUG: https://github.com/vercel/next.js/issues/52842
+
+/**
+ * `intercepting route`를 위한 parallel 경로에서 정적 페이지 생성이 되지 않는 이슈가 존재한다.
+ *
+ * 이러한 상황에서 이미지 데이터(너비, 높이)를 사전에 불러와 활용하는 이미지 최적화 작업이 매 요청마다 이루어질 경우,
+ * 로딩시간이 매우 오래 걸리기 때문에 이미지 최적화 옵션을 비활성화 처리함.
+ */
 export default async function ReadmeModal({
   params: { slug = [] },
 }: {
@@ -17,10 +27,27 @@ export default async function ReadmeModal({
   return (
     <RouteModal>
       <ModalLayout>
-        <ReadmeHeader owner={owner} repo={repo} />
-        <ScrollYWithPaddingLayout>
-          <ReadmeContent readme={readme} />
-        </ScrollYWithPaddingLayout>
+        {!readme ? (
+          <p>레포지토리가 존재하지 않습니다.</p>
+        ) : (
+          (() => {
+            const markdown = utf8.decode(base64.decode(readme.content));
+
+            return (
+              <>
+                <ReadmeHeader owner={owner} repo={repo} />
+
+                <ScrollYWithPaddingLayout>
+                  <Markdown
+                    markdown={markdown}
+                    imageOptimize={false}
+                    imageInline={true}
+                  />
+                </ScrollYWithPaddingLayout>
+              </>
+            );
+          })()
+        )}
       </ModalLayout>
     </RouteModal>
   );
--- src/app/portfolio/readme/[[...slug]]/page.tsx
+++ src/app/portfolio/readme/[[...slug]]/page.tsx
@@ -1,7 +1,16 @@
-import { ReadmeContent } from "./_components/ReadmeContent";
 import { apiService } from "@/apis";
+import { Markdown } from "@/components/core/Markdown";
 import Link from "next/link";
+import base64 from "base-64";
+import utf8 from "utf8";
 
+/**
+ * 블로그 데이터를 저장하고 있는 레포지토리가 아닌, 다른 레포지토리의 데이터(readme)를 조회하는 페이지이기 때문에,
+ * 동적 라우트로 페이지가 구성이 된다.
+ *
+ * 이러한 상황에서 이미지 데이터(너비, 높이)를 사전에 불러와 활용하는 이미지 최적화 작업이 매 요청마다 이루어질 경우,
+ * 로딩시간이 매우 오래 걸리기 때문에 이미지 최적화 옵션을 비활성화 처리함.
+ */
 export default async function ReadmePage({
   params: { slug = [] },
 }: {
@@ -10,6 +19,12 @@ export default async function ReadmePage({
   const [owner, repo] = slug;
 
   const readme = await apiService.getRepositoryReadme(owner, repo);
+
+  if (!readme) {
+    return <p>레포지토리가 존재하지 않습니다.</p>;
+  }
+
+  const markdown = utf8.decode(base64.decode(readme.content));
 
   return (
     <>
@@ -22,7 +37,7 @@ export default async function ReadmePage({
         </Link>
       </div>
 
-      <ReadmeContent readme={readme} />
+      <Markdown markdown={markdown} imageOptimize={false} imageInline={true} />
     </>
   );
 }
