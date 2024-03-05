cc30850944d080854791f7f6519c44c59782a374
junghyunbak <jeong5728@gmail.com>
2024-03-05 22:02:00 +0900

    feat: comment 페이지 메타 데이터 추가

--- src/app/post/comment/[id]/page.tsx
+++ src/app/post/comment/[id]/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { ResponsivePaddingLayout } from "@/components/layout/ResponsivePaddingLayout";
 import { Markdown } from "@/components/core/Markdown";
 import { imageUtils } from "@/utils";
+import { type Metadata } from "next";
 
 export async function generateStaticParams() {
   const allIssueComment = await apiService.getAllIssueComment();
@@ -11,6 +12,24 @@ export async function generateStaticParams() {
   return allIssueComment.map((issueComment) => ({
     id: issueComment.id.toString(),
   }));
+}
+
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const issueComment = await apiService.getAnIssueComment(id);
+
+  const issueNumber =
+    (/https:\/\/github.com\/junghyunbak\/junghyunbak\/issues\/([0-9]+)/.exec(
+      issueComment?.html_url || ""
+    ) || [])[1];
+
+  return {
+    title: `${issueNumber}번 게시글 댓글 - ${id} | 개발자 박정현`,
+    description: (issueComment?.body || "").slice(0, 80),
+  };
 }
 
 export default async function PostComment({
